b3cdbfac502389d867d5b13c836fdeb054eb16ca
LacamJC <joaoramajo744@gmail.com>
2025-07-24 18:22:20 -0300

    feat: implementa abilities a nivel de rotas por middlewares

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,14 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return ApiResponse::success('API is running');
     });
 
-    Route::apiResource('clients', ClientController::class);
+    // Route::apiResource('clients', ClientController::class);
+    Route::prefix('clients')->group(function () {
+        Route::get('/', [ClientController::class, 'index'])->middleware('ability:clients:list');
+        Route::post('/', [ClientController::class, 'store']);
+        Route::get('/{client}', [ClientController::class, 'show'])->middleware('ability:clients:detail');
+        Route::put('/{client}', [ClientController::class, 'update']);
+        Route::delete('/{client}', [ClientController::class, 'delete']);
+    });
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
