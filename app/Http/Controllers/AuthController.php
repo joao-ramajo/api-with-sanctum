6e431bc9091ea6916d0fadd2a02d6c76e98c3eb8
LacamJC <joaoramajo744@gmail.com>
2025-07-24 16:55:30 -0300

    feat: implementa autenticação com login para usuários

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // valida a requisição
+        $request->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]
+        );
+
+        // login
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $attempt = auth()->attempt(
+            [
+                'email' => $email,
+                'password' => $password
+            ]
+        );
+
+        if (!$attempt) {
+            return response()
+                ->json(
+                    [
+                        'status' => 'ok',
+                        'error' => 'Unauthorized'
+                    ],
+                    401
+                );
+        }
+
+        // authenticate user
+
+        $user =  auth()->user();
+
+        $token = $user->createToken($user->name)->plainTextToken;
+
+        // retorna o token da api 
+
+        return response()
+            ->json(
+                [
+                    'token' => $token
+                ],
+                200
+            );
+    }
+    public function logout() {}
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -7,5 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/status', function () {
     return response()->json(['status' => 'ok', 'message' => 'Server is running'], 200);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::apiResource('clients', ClientController::class);
